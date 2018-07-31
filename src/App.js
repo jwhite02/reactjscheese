d6194255d10c4c5fd55def5278125d4753cdbf2a
jwhite02 <johnwhite4837@gmail.com>
2018-07-31 14:00:52 -0400

    Added build folder and changed Router to HashRouter

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import '../src/styles/css/App.css';
 //import HeaderSection from '../src/components/header/HeaderSection';
 //import CardSection from '../src/components/content/CardSection';
 //import FooterSection from '../src/components/footer/FooterSection';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 //import { Switch } from 'react-router-dom';
 //import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 //import SiteHolder from './components/content/SiteHolder';
@@ -18,11 +18,11 @@ import ScrollToTop from './components/ScrollToTop';
 class App extends Component {
   render() {
     return <div>
-        <Router>
+        <HashRouter>
           <ScrollToTop>
             <SiteBugerMenu />
           </ScrollToTop>
-        </Router>
+        </HashRouter>
       </div>;
       /* <div>  <HeaderSection /> <CardSection /> <FooterSection />
         <HeaderSection />
